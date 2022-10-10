04eca3f212b53f81ff45ce8af9783fccb351a61c
Tobias Laufersweiler <me@tobiascodes.de>
2022-10-10 19:32:09 +0200

    update config

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const app = express()
 
 const PORT = 5000
 
-app.get('/', (req, res) => res.send('hello world'))
+app.get('/', (req, res) => res.json({ hello: 'world' }))
 
 app.listen(PORT, () => {
 	console.log(`Listening on http://localhost:${PORT}`)
