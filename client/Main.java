8d48ae539449526796b22daeacd34eca1138fd01
Tobias Laufersweiler <me@tobiascodes.de>
2022-10-10 20:45:25 +0200

    Update Main.java

--- client/Main.java
+++ client/Main.java
@@ -1,10 +1,6 @@
 class Main {
   public static void main(String[] args) throws InterruptedException {
-    int i = 0;
-    while(true) {
-      System.out.println(i);
-      i++;
-      Thread.sleep(2000);
-    }
+    System.out.println("Java Client started...");
+    while(true) {}
   }
 }
