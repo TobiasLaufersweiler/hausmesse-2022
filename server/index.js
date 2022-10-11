08e197f3a5dd90af1024f406c86978c23118e2f9
laufeto <me@tobiascodes.de>
2022-10-11 10:04:32 +0200

    update server

--- server/index.js
+++ server/index.js
@@ -5,6 +5,26 @@ const PORT = 5000
 
 app.get('/', (req, res) => res.json({ hello: 'world' }))
 
+app.post('/data', (req, res) => {
+	const data = req.body
+	console.log(data)
+
+	// Save data to DB
+
+	// Broadcast data via Socket.IO
+})
+
+app.get('/data', (req, res) => {
+	res.json([
+		{
+			id: 1,
+			temperature: 23.1,
+			humidity: 67,
+			timestamp: Date.now(),
+		},
+	])
+})
+
 app.listen(PORT, () => {
 	console.log(`Listening on http://localhost:${PORT}`)
 })
