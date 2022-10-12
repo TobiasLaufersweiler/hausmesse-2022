41ad08da61c93989b48844cbbadacb382d464a86
Marc <kingfhx@gmail.com>
2022-10-12 17:27:21 +0200

    Client

---
+++ client/Client/src/client/Client.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package client;
+
+import client.driver.DHT22;
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.Pin;
+import com.pi4j.io.gpio.PinState;
+import com.pi4j.io.gpio.RaspiPin;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author murawia
+ */
+public class Client {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws MalformedURLException, IOException {
+        GpioController gpio = GpioFactory.getInstance();
+        GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "MyLED", PinState.HIGH);
+        
+        pin.setShutdownOptions(true, PinState.LOW);
+        DHT22 driver = new DHT22((Pin) pin);
+        try {
+            while(driver.read()){
+                sendData(driver.getTemperature(), driver.getHumidity(), new Date());
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
+    }
+    private static void sendData(double temp, double humanity, Date timestamp) throws MalformedURLException, ProtocolException, IOException{
+        URL url = new URL("https://localhost/api/data/");
+        URLConnection con = url.openConnection();
+        HttpURLConnection http = (HttpURLConnection)con;
+        http.setRequestMethod("POST"); // PUT is another valid option
+        http.setDoOutput(true);
+        
+        byte[] out = "{\"temp\":\"1\",\"humanity\":\"12\",\"timestamp\":\"timestampxxy\"}".replace("timestampxxy", new Date().toString()) .getBytes(StandardCharsets.UTF_8);
+        int length = out.length;
+
+       http.setFixedLengthStreamingMode(length);
+       http.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+       http.connect();
+       try(OutputStream os = http.getOutputStream()) {
+          os.write(out);
+}
+    }
+    
+}
